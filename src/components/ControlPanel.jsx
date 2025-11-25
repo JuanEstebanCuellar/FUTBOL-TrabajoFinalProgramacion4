acd997942d42bf8b68a6a39d47416bfd520ee79d
manuellargo890-coder <manuellargo890@gmail.com>
2025-11-24 21:15:24 -0500

    ControlPanel JSX

---
+++ src/components/ControlPanel.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import '../styles/ControlPanel.css';
+
+function ControlPanel({
+    rowColors,
+    onRowColorsChange,
+    showFavoritesOnly,
+    onShowFavoritesOnlyChange,
+    darkMode,
+    onDarkModeToggle
+}) {
+    return (
+        <div className="control-panel">
+            <div className="control-panel__section">
+                <label>Colorear filas:</label>
+                <select
+                    value={rowColors}
+                    onChange={(e) => onRowColorsChange(e.target.value)}
+                >
+                    <option value="ninguno">Ninguno</option>
+                    <option value="pares">Pares</option>
+                    <option value="impares">Impares</option>
+                </select>
+            </div>
+            <div className="control-panel__section">
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={showFavoritesOnly}
+                        onChange={(e) => onShowFavoritesOnlyChange(e.target.checked)}
+                    />
+                    Mostrar solo favoritos
+                </label>
+            </div>
+            <div className="control-panel__section">
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={darkMode}
+                        onChange={(e) => onDarkModeToggle(e.target.checked)}
+                    />
+                    Modo oscuro
+                </label>
+            </div>
+        </div>
+    );
+}
+export default ControlPanel;
